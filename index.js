aab792b4459f34bede8986ea2fcfd5ce33db1578
Ali Furkan Demirbaş <alistefurkan@hotmail.com>
2020-12-14 06:33:36 +0300

    second pug and favicon commit

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 const express = require('express');
-
+const path = require('path');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const favicon = require('serve-favicon');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(helmet());
+app.use(morgan('tiny'));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
 app.get('/', (req, res) => {
-  res.send(`
-  <h1>🍋 Welcome to the Landing Page! 🍊</h1>
-  <h2> Work in Progress (obviously) </h2>  
-   `);
+  res.render('index', { title: 'Home' });
 });
 
 app.listen(PORT, () => {
